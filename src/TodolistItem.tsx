e3b8049c582bf60197f1110bc7e73666bd286bd7
AliakseiRedzko <aleha.redko@gmail.com>
2025-12-18 17:15:27 +0300

    work under react iTincubator

---
+++ src/TodolistItem.tsx
@@ -0,0 +1,36 @@
+type Props = {
+    title: string
+    subTitle: string
+    description: string
+    tasks: Task[]
+}
+
+export const TodolistItem =  ({ title, tasks }:Props)=>{
+
+    return (
+
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={true}/> <span>JS</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={false}/> <span>React</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
